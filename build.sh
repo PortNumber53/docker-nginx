fb24e016751b5f0c25b91c087f8dad859a5eed05
Mauricio S Otta <mauricio@portnumber53.com>
2017-02-15 19:13:16 +0000

    Moved TAG variable to top

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 ORIGINAL_DOCKERFILE=Dockerfile.nginx.original
 ADDON_DOCKER_FILE=Dockerfile.addon
+TAG=`date +"%Y-%m-%d-%H-%M-%S"`
 
 curl -o ${ORIGINAL_DOCKERFILE} https://raw.githubusercontent.com/nginxinc/docker-nginx/e950fa7dfcee74933b1248a7fe345bdbc176fffb/mainline/alpine/Dockerfile
 
@@ -13,7 +14,6 @@ cat ${ORIGINAL_DOCKERFILE} | sed 's~EXPOSE 80 443~'"${ESCAPED_DATA}"'~' > Docker
 
 sed -i 's/MAINTAINER NGINX Docker Maintainers "docker-maint@nginx.com"/MAINTAINER Me Grimlock "grimlock@portnumber53.com"/g' Dockerfile
 
-TAG=`date +"%Y-%m-%d-%H-%M-%S"`
 docker build -t portnumber53/docker-nginx:${TAG} . \
   && docker push portnumber53/docker-nginx:${TAG} \
   && echo "Pushed image successfuly."
